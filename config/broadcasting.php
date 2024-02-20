171bf40cebf76179cd287c0d83ec19c0f0548501
Almasyriqi <ikramalmasyriqi@gmail.com>
2024-02-20 16:27:21 +0700

    update config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => false,
+                'encrypted' => true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
--- config/websockets.php
+++ config/websockets.php
@@ -29,8 +29,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'path' => env('PUSHER_APP_PATH'),
             'capacity' => null,
-            'enable_client_messages' => false,
+            'enable_client_messages' => true,
             'enable_statistics' => true,
+            'encrypted' => true,
         ],
     ],
 
