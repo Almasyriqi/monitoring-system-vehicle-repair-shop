0b3400a38c6e5f42f2606c8c60e6d4b4a245e153
Almasyriqi <ikramalmasyriqi@gmail.com>
2024-02-20 16:44:00 +0700

    change config to http

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'scheme' => 'http',
                 'encrypted' => true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
